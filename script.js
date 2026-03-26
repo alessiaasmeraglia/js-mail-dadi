c33513b5c656fe222001368b5689a2d93d6565cc
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-03-26 15:17:14 +0100

    Concluso secondo esercizio

--- script.js
+++ script.js
@@ -33,3 +33,30 @@ if (emailTrovata) {
     console.log("Accesso non consentito");
     alert("Accesso non consentito");
 }
+
+// =========================
+// ESERCIZIO 2: GIOCO DEI DADI
+// =========================
+
+// Genero un numero random da 1 a 6 per il giocatore
+const dadoGiocatore = Math.floor(Math.random() * 6 + 1);
+// Genero un numero random da 1 a 6 per il computer
+const dadoComputer = Math.floor(Math.random() * 6 + 1);
+
+// Stampo i risultati dei tiri
+console.log("Tiro del giocatore:", dadoGiocatore);
+console.log("Tiro del computer:", dadoComputer);
+
+// Stabilisco il vincitore
+if (dadoGiocatore > dadoComputer){
+    console.log("Hai vinto!");
+    alert("Giocatore: " + dadoGiocatore + " | Computer: " + dadoComputer + " Hai vinto!");
+}   else if (dadoComputer > dadoGiocatore) {
+    console.log("Ha vinto il computer!");
+    alert("Giocatore: " + dadoGiocatore + " | Computer: " + dadoComputer + "  Ha vinto il computer!");
+} else {
+    console.log("Pareggio!");
+    alert("Giocatore: " + dadoGiocatore + " | Computer: " + dadoComputer + " Pareggio!");
+}
+
+
