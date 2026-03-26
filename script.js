f12e2e67873e5f6a310c204700fb459762875642
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-03-26 15:03:26 +0100

    Completato primo esercizio

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ const listaInvitati = [
     "giulia@outlook.com",
     "paolo@email.com"
 ];
+
+// Chiedo all'utente la sua email
+const emailUtente = prompt("Inserisci la tua email:");
+
+// Creo una variabile di controllo
+let emailTrovata = false;
+
+// Controllo con un ciclo for se l'email dell'utente è presente nella lista
+for (let i = 0; i < listaInvitati.length; i++) {
+    if (emailUtente === listaInvitati[i]) {
+        emailTrovata = true;
+        break;
+    }
+}
+
+// Stampo un messaggio in base al risultato del controllo
+if (emailTrovata) {
+    console.log("Accesso consentito");
+    alert("Accesso consentito");
+} else {
+    console.log("Accesso non consentito");
+    alert("Accesso non consentito");
+}
