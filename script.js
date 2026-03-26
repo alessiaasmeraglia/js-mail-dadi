df0c2f8d7ff9582292e3e824cee40862142680de
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-03-26 14:40:15 +0100

    Inizio primo esercizio

---
+++ script.js
@@ -0,0 +1,12 @@
+// =========================
+// ESERCIZIO 1: MAIL
+// =========================
+
+// Creo una lista di email invitate alla festa
+const listaInvitati = [
+    "mario@gmail.com",
+    "luca@hotmail.com",
+    "anna@yahoo.com",
+    "giulia@outlook.com",
+    "paolo@email.com"
+];
